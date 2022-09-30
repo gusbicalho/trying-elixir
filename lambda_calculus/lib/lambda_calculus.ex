4e4b2bb47c41328dc7d4b73286121bcefa28c6f2
Gustavo Bicalho <gustavo@liveflow.io>
2022-09-30 12:11:51 -0300

    add scope analysis to annotate lookups wiht :global or :local

--- lambda_calculus/lib/lambda_calculus.ex
+++ lambda_calculus/lib/lambda_calculus.ex
@@ -8,7 +8,8 @@ defmodule LambdaCalculus do
   def test_stmt(s, source_name \\ nil) do
     with {{:ok, stmt}, ""} <-
            LambdaCalculus.Pipeline.TextToParseTree.parse_stmt(s, source_name: source_name),
-         {:ok, stmt} <- LambdaCalculus.Pipeline.ParseTreeToAST.Statement.parse(stmt) do
+         {:ok, stmt} <- LambdaCalculus.Pipeline.ParseTreeToAST.Statement.parse(stmt),
+         stmt = LambdaCalculus.Pipeline.ASTAnalysis.Scope.analyze(stmt) do
       stmt
     end
   end
@@ -23,10 +24,16 @@ defmodule LambdaCalculus do
               function: %AST.Expression{
                 expression: %AST.Application{
                   function: %AST.Expression{
-                    expression: %AST.Lookup{lookup: %AST.Identifier{name: :plus}}
+                    expression: %AST.Lookup{
+                      lookup: %AST.Identifier{name: :plus},
+                      meta: %{scope: :global}
+                    }
                   },
                   argument: %AST.Expression{
-                    expression: %AST.Lookup{lookup: %AST.Identifier{name: :q}}
+                    expression: %AST.Lookup{
+                      lookup: %AST.Identifier{name: :q},
+                      meta: %{scope: {:local, 0}}
+                    }
                   }
                 }
               },
@@ -49,7 +56,8 @@ defmodule LambdaCalculus do
             parameter: %AST.Identifier{name: :a},
             body: %AST.Expression{
               expression: %AST.Lookup{
-                lookup: %AST.Identifier{name: :a}
+                lookup: %AST.Identifier{name: :a},
+                meta: %{scope: {:local, 0}}
               }
             }
           }
---
+++ lambda_calculus/lib/lambda_calculus/pipeline/ast_analysis/scope.ex
@@ -0,0 +1,57 @@
+defmodule LambdaCalculus.Pipeline.ASTAnalysis.Scope do
+  alias LambdaCalculus.Pipeline.AST
+
+  def analyze(%AST.Statement{} = stmt) do
+    analyze_stmt(stmt, [])
+  end
+
+  defp analyze_stmt(%AST.Statement{} = stmt, locals) do
+    update_in(stmt.statement, fn
+      %AST.Declaration{} = decl -> analyze_decl(decl, locals)
+      %AST.Expression{} = expr -> analyze_expr(expr, locals)
+    end)
+  end
+
+  defp analyze_decl(%AST.Declaration{} = decl, locals) do
+    update_in(decl.definition, &analyze_expr(&1, locals))
+  end
+
+  defp analyze_expr(%AST.Expression{} = expr, locals) do
+    update_in(expr.expression, fn
+      %AST.Lambda{} = lambda -> analyze_lambda(lambda, locals)
+      %AST.Application{} = application -> analyze_application(application, locals)
+      %AST.Lookup{} = lookup -> analyze_lookup(lookup, locals)
+      %AST.Literal{} = literal -> literal
+    end)
+  end
+
+  defp analyze_lambda(%AST.Lambda{} = lambda, locals) do
+    %AST.Identifier{name: param_name} = lambda.parameter
+    locals = [param_name | locals]
+    update_in(lambda.body, &analyze_expr(&1, locals))
+  end
+
+  defp analyze_application(%AST.Application{} = application, locals) do
+    application
+    |> update_in([Access.key!(:function)], &analyze_expr(&1, locals))
+    |> update_in([Access.key!(:argument)], &analyze_expr(&1, locals))
+  end
+
+  defp analyze_lookup(%AST.Lookup{} = lookup, locals) do
+    %AST.Lookup{lookup: %AST.Identifier{name: lookup_name}} = lookup
+
+    update_in(
+      lookup.meta,
+      fn meta ->
+        Map.put_new(
+          meta,
+          :scope,
+          case Enum.find_index(locals, &(&1 === lookup_name)) do
+            nil -> :global
+            de_brujn_index -> {:local, de_brujn_index}
+          end
+        )
+      end
+    )
+  end
+end
